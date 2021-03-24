766230e1577b9b577a495c3b9e7f9bee9364bd44
Mark Francisco <mfrancisco581@gmail.com>
2021-03-24 15:01:14 -0500

    Add single file Vue components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <h1>Good day, {{ name }}!</h1>
+    <!-- <button>Submit!</button> -->
+
+    <!-- you should bind by "product.id" when using v-bind:key -->
+    <ul v-for="product in products" v-bind:key="product.id">
+      <h4>{{ product.name }}</h4>
+      <p>{{ product.price }}</p>
+      <img v-bind:src="product.image_url" v-bind:alt="product.name" />
+    </ul>
   </div>
 </template>
 
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      name: "Mark",
+      products: [],
+    };
+  },
+  created: function () {
+    // "this" is set to the VueComponent object
+    this.indexProducts();
+  },
+  methods: {
+    // use the traditional fn syntax for defining fn's that contain axios HTTP requests
+    indexProducts: function () {
+      axios.get("/api/products").then((response) => {
+        // populate the products array defined in this Vue file
+        // you're returning the json data from your simple_mini_capstone Rails views.
+        this.products = response.data;
+        // "this" is set to the VueComponent object
+        console.log("all of your products:", this.products);
+      });
+    },
   },
 };
 </script>
